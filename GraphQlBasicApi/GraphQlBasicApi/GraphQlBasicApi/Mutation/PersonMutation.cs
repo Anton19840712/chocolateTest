f9321ba2753b8661c064ba61846ebef7ea65446b
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-25 12:00:48 +0300

    Delete extra usings.

--- GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Mutation/PersonMutation.cs
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Mutation/PersonMutation.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GraphQL;
+﻿using GraphQL;
 using GraphQL.Types;
 using GraphQlBasicApi.Interfaces;
 using GraphQlBasicApi.Models;
--- GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Schema/PersonSchema.cs
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Schema/PersonSchema.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GraphQlBasicApi.Mutation;
+﻿using GraphQlBasicApi.Mutation;
 using GraphQlBasicApi.Query;
 
 namespace GraphQlBasicApi.Schema
