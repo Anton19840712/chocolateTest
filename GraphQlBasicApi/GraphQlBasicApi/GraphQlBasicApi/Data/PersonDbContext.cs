4bc85207d32dfb71c9cb3f4805f481f89890f62f
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-25 11:23:05 +0300

    Add db context.

---
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Data/PersonDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GraphQlBasicApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQlBasicApi.Data
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Products { get; set; }
+    }
+}
