1fae77c4b5d9c2411aba86921588f5cb3ab4b4ea
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-25 11:26:01 +0300

    Add abstraction.

---
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Interfaces/IPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQlBasicApi.Models;
+
+namespace GraphQlBasicApi.Interfaces
+{
+    interface IPerson
+    {
+        List<Person> GetAllPersons();
+        Person AddPerson(Person person);
+        Person UpdatePerson(int id, Person person);
+        void DeletePerson(int id);
+        Person GetPersonById(int id);
+    }
+}
