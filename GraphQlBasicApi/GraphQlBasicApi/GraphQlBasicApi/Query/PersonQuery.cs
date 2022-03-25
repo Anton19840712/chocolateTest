937e74b84c510c43dab26267c073c7249fba7682
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-25 11:43:59 +0300

    Add query.

---
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Query/PersonQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using GraphQlBasicApi.Interfaces;
+using GraphQlBasicApi.Type;
+
+namespace GraphQlBasicApi.Query
+{
+    public class PersonQuery : ObjectGraphType
+    {
+        public PersonQuery(IPerson personService)
+        {
+            Field<ListGraphType<PersonType>>("persons", resolve: context => personService.GetAllPersons());
+
+            Field<PersonType>("person", arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" }),
+
+                resolve: context => personService.GetPersonById(context.GetArgument<int>("id")));
+        }
+    }
+}
