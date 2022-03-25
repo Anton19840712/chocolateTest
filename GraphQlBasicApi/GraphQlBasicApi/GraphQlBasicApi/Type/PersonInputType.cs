63b48d715eb72d53206b232b5cc7b745c2855c04
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-25 11:38:31 +0300

    Add types.

---
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Type/PersonInputType.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQlBasicApi.Type
+{
+    public class PersonInputType : InputObjectGraphType
+    {
+        public PersonInputType()
+        {
+            Field<IntGraphType>("id");
+            Field<StringGraphType>("name");
+            Field<FloatGraphType>("country");
+            Field<FloatGraphType>("score");
+            Field<FloatGraphType>("phone");
+        }
+    }
+}
---
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Type/PersonType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using GraphQlBasicApi.Models;
+
+namespace GraphQlBasicApi.Type
+{
+    public class PersonType : ObjectGraphType<Person>
+    {
+        public PersonType()
+        {
+            Field<IntGraphType>("id");
+            Field<StringGraphType>("name");
+            Field<FloatGraphType>("country");
+            Field<FloatGraphType>("score");
+            Field<FloatGraphType>("phone");
+        }
+    }
+}
