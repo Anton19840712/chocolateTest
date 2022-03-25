54ae2b02a3055c4382219c98c7ad3e279a05c5d3
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-25 11:44:18 +0300

    Add schema.

---
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Schema/PersonSchema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQlBasicApi.Mutation;
+using GraphQlBasicApi.Query;
+
+namespace GraphQlBasicApi.Schema
+{
+    public class PersonSchema : GraphQL.Types.Schema
+    {
+        public PersonSchema(PersonQuery personQuery, PersonMutation personMutation)
+        {
+            Query = personQuery;
+
+            Mutation = personMutation;
+        }
+    }
+}
