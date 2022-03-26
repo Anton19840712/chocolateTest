2088025055d8087857685505389effd83a75b528
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-27 01:42:38 +0300

    Delete extra lines.

--- Demo/Demo/Data/PersonContext.cs
+++ Demo/Demo/Data/PersonContext.cs
@@ -4,23 +4,8 @@ namespace Demo.Data
 {
     public class PersonContext : DbContext
     {
-        public PersonContext(DbContextOptions options) : base(options)
-        {
-
-        }
+        public PersonContext(DbContextOptions options) : base(options) { }
         public DbSet<Person>? Persons { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(@"Server=GRIDUSHKO-AA\\SQLEXPRESS01;Database=Sales;Trusted_Connection=True;MultipleActiveResultSets=true");
-            }
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Person>();
-        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
     }
 }
