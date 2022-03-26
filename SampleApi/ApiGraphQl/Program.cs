44c37b7174f927ffdfcf925ba8ee0d93fc5f272c
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-27 01:17:19 +0300

    Delete extra lines.

--- SampleApi/ApiGraphQl/Program.cs
+++ SampleApi/ApiGraphQl/Program.cs
@@ -1,20 +1,22 @@
-using ApiGraphQl;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-public class Program
+namespace ApiGraphQl
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        CreateHostBuilder(args).Build().Run();
-    }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
 
 
