a5a6e2e5454e369275f77fcba4008232ab5eaf60
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-27 00:24:50 +0300

    Delete extra lines.

--- GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Data/PersonDbContext.cs
+++ GraphQlBasicApi/GraphQlBasicApi/GraphQlBasicApi/Data/PersonDbContext.cs
@@ -11,10 +11,6 @@ namespace GraphQlBasicApi.Data
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(@"Data Source=GRIDUSHKO-AA\SQLEXPRESS01;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            }
         }
         public DbSet<Person> Persons { get; set; }
     }
