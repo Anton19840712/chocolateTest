4bba4d11512a063ebf6daaee75de6cfcadcb2513
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-27 01:15:51 +0300

    Delete extra lines.

--- SampleApi/ApiGraphQl/Context/PersonContext.cs
+++ SampleApi/ApiGraphQl/Context/PersonContext.cs
@@ -5,23 +5,13 @@ namespace ApiGraphQl.Context
 {
     public class PersonContext : DbContext
     {
-        public PersonContext(DbContextOptions options) : base(options)
+        public PersonContext(DbContextOptions<PersonContext> options) : base(options)
         {
 
         }
-        public DbSet<Person>? Persons { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(@"Server=GRIDUSHKO-AA\\SQLEXPRESS01;Database=Sales;Trusted_Connection=True;MultipleActiveResultSets=true");
-            }
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Person>();
-        }
+        public DbSet<Person>? Persons { get; set; }
     }
 }
