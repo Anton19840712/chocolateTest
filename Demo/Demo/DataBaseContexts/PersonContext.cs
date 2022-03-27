58e8a73d3339258d64a78aea39d8ac0574362c7b
Anton Anatolievich Gridushko <anton.griduyshko@yahoo>
2022-03-27 19:33:57 +0300

    Delete nullable elements.

--- Demo/Demo/DataBaseContexts/PersonContext.cs
+++ Demo/Demo/DataBaseContexts/PersonContext.cs
@@ -3,10 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Demo.DataBaseContexts
 {
+    /// <summary>
+    /// Database context for EF Core.
+    /// </summary>
     public class PersonContext : DbContext
     {
         public PersonContext(DbContextOptions options) : base(options) { }
-        public DbSet<PersonDalDto>? Persons { get; set; }
+        public DbSet<PersonDalDto> Persons { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
     }
 }
--- Demo/Demo/Models/PersonModels/DalPersonsDto/PersonDalDto.cs
+++ Demo/Demo/Models/PersonModels/DalPersonsDto/PersonDalDto.cs
@@ -3,15 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Demo.Models.PersonModels.DalPersonsDto
 {
+    /// <summary>
+    /// Dal model to communicate with database.
+    /// </summary>
     public class PersonDalDto
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? Gender { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
         public int Score { get; set; }
     }
 }
